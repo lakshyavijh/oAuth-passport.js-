1be3dfd13dc40f20eb83eeaa22621a10cdac13c7
lakshyavijh <lakshya.vijh99@gmail.com>
2020-04-03 11:18:06 +0530

    setting up routes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const authRoutes =require('./routes/auth-routes');
 const app=express();
 
 //setup view engine
 app.set('view engine','ejs');
+
+//setup routes
+app.use('/auth',authRoutes);
+
 
 //create home route
 app.get('/',(req,res) => {
